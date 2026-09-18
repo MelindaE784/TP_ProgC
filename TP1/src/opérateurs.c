40dff45cf8a2f7c66fdc22ed3ff553172444988c
MelindaE784 <eloymelinda@gmail.com>
2026-09-18 13:13:37 +0000

    Opérateurs

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int a = 16;
+    int b = 3;
+
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b (reste) = %d\n", a % b);
+
+    printf("a == b ? %d\n", a == b);
+    printf("a > b ? %d\n", a > b);
+
+    return 0;
+}
 

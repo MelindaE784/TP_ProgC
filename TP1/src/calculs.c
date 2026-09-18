27b7b1e2a27a569b28ffdee3381dc1d9c8e2dfdc
MelindaE784 <eloymelinda@gmail.com>
2026-09-18 13:24:11 +0000

    Calculatrice

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+int main() {
+    int num1 = 12;
+    int num2 = 5;
+    char op = '+';
+
+    switch (op) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            break;
+        case '%':
+            printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            break;
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);
+            break;
+        default:
+            printf("Operateur inconnu !\n");
+    }
+
+    return 0;
+}
